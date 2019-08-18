6f0705c31ae901aa439b45c07223cd073e3eec80
root <root@localhost.localdomain>
2019-08-18 03:17:48 -0400

    add soal casting

---
+++ python/soalCasting.py
@@ -0,0 +1,10 @@
+#soal casting rubah tipe data variable
+
+text = "5000" #rubah jadi float
+intag = 4000 #rubah jadi string
+floate = 60000.234 #rubah jadi int
+
+#print dan gunakan validasi type()
+
+
+
