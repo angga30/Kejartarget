d483430944492d30138da2485c241d7d3436bd1e
root <root@localhost.localdomain>
2019-08-18 03:09:14 -0400

    tambah casting

---
+++ python/casting.py
@@ -0,0 +1,22 @@
+#casting adalah metode merubah sebuah type variable ke type variable lainya 
+#di python ada 3 metode built in 
+#int()
+#float()
+#str()
+
+integ = 1
+stringa = "123"
+floata = 2.9
+
+#penggunaan str()
+#print(stringa+integ)
+strInt = str(integ)
+strString = str(stringa)
+strfloata = str(floata)
+
+print(strInt)
+print(strString)
+print(strfloata)
+
+print(int(stringa))
+print(int(floata))
