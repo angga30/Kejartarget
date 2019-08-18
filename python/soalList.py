9225da2f2ffe902016942d659b625a546565a2c9
root <root@localhost.localdomain>
2019-08-18 04:48:41 -0400

    add data soallist

---
+++ python/soalList.py
@@ -0,0 +1,10 @@
+# buat list berisi ["satu","dua","tiga",4]
+
+#lalu tambahkan data "lima" ke list yang sudah di buat
+
+#tambahkan  data "enam" di index ke 2
+
+# hapus data "satu" di list
+
+# copy list yang sudah di buat ke variable dengan nama listNew
+
