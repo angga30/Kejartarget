482a77076b675d3a1b73be9a32f80b21bd2f9715
root <root@localhost.localdomain>
2019-08-18 03:46:59 -0400

    add soal operator

---
+++ python/soalOperator.py
@@ -0,0 +1,16 @@
+#soal operator 
+
+awsd = "1500"
+awd = 15000
+aws =1500.50
+
+#modulus var awsd dengan aws
+
+
+
+#jumlahkan var awd dan aws
+
+
+#exponensiasi var awsd dan awd
+
+#aws+(awd/awsd)*awd tuliskan dalam notasi python nya
